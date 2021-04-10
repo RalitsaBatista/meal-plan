a62820b096af24c913bddfdf01b6de72fa69ff10
s2000574 <Ralitsa.Batista@edu.bc.fi>
2021-04-10 22:21:08 +0300

    Edit app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,16 +16,7 @@ export class AppComponent {
   title= "Meal Planner";
   [x: string]: any;
   constructor(public translate: TranslateService, private foodService: FoodService, private router:Router , public dialog: MatDialog, public auth: AuthService) {
-    /*
-    translate.addLangs(['en', 'fi', 'es', 'bg' ]);
-    translate.setDefaultLang('en');
-    const browserLang = translate.getBrowserLang();
-    //it will check for language match and if it doesn't find then it will translate to the default lang ('en')
-    translate.use(browserLang.match(/en|fi|es|bg/) ? browserLang : 'en');
-    //translate.use('fi');
-    //selectLang
-    */
-    //console.log('LANGO',translate.getTransLanguage());
+
   } 
   foods: Food[] = [];
 
@@ -36,7 +27,7 @@ export class AppComponent {
   goToBaby(pageName:string):void{
     this.router.navigate([`$(pageName)`]);
   }
-  
+
   goToInfo(pageName:string):void{
     this.router.navigate([`$(pageName)`]);
   }
